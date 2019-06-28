11f73e94c54214b9a36b41ce1f4d222fd4865944
Edoardo Olivieri <diddo.42@hotmail.it>
2019-06-28 17:13:57 +0100

    added fixed-top navbar and some content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import './style/App.scss';
+import { Sticky, StickyContainer  } from 'react-sticky';
 
 import Home from './components/home';
 import Service from './components/service';
@@ -8,7 +9,7 @@ import Brands from './components/brands';
 
 const Section = styled.div`
   margin: 50px 60px;
-  // min-height: 100vh;
+  min-height: 100vh;
 `
 
 const AppWrapper = styled.div`
@@ -27,28 +28,35 @@ const Nav = styled.div`
     margin: 0px 10px;
   }
 `
+let renderCount = 0;
 function App() {
   return (
     <AppWrapper>
-      <Navbar>
-        <div className="title">
-          Dixit
-        </div>
-        <Nav>
-          <p>Brands</p>
-          <p>Services</p>
-          <p>About Us</p>
-        </Nav>
-      </Navbar>
-      <Section>
-        <Home />
-      </Section>
-      <Section>
-        <Service />
-      </Section>
-      <Section>
-        <Brands />
-      </Section>
+      <StickyContainer>
+        <Sticky disableHardwareAcceleration>
+        {({ style }) => (
+          <Navbar style={style} renderCount={renderCount++}>
+            <div className="title">
+              Dixit
+            </div>
+            <Nav>
+              <p>Brands</p>
+              <p>Services</p>
+              <p>About Us</p>
+            </Nav>
+          </Navbar>
+        )}
+        </Sticky>
+        <Section>
+          <Home />
+        </Section>
+        <Section>
+          <Service />
+        </Section>
+        <Section>
+          <Brands />
+        </Section>
+      </StickyContainer>
     </AppWrapper>
   );
 }

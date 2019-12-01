fcbd64dedfc92bf86ee938c95ba3b937b7a217ea
Edoardo Olivieri <diddo.42@hotmail.it>
2019-12-01 16:40:26 +0000

    fix

--- src/components/stockx/Stockx.jsx
+++ src/components/stockx/Stockx.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react"
 import { Container } from "react-bootstrap"
 
 import Navbar from "../navbar/Navbar.jsx"
-
 export default class Home extends Component{
     render(){
         return(

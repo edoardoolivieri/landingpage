b99b2a828d2e5fcc481c6db44bf13c1315ba3501
Edoardo Olivieri <diddo.42@hotmail.it>
2020-04-13 15:25:34 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/App.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
